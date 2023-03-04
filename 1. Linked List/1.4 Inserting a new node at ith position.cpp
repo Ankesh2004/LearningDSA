3467b259e40978c09ce4ed38b7a95ecd0eed05ac
Ankesh Gupta <65663130+Ankesh2004@users.noreply.github.com>
2023-03-04 20:19:32 +0530

    Create 1.4 Inserting a new node at ith position.cpp

---
+++ 1. Linked List/1.4 Inserting a new node at ith position.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+class Node{
+public:
+    int data;
+    Node *next;
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+
+void print(Node *head){
+    Node *temp = head;
+    
+
+    while(temp != NULL){
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
+Node *takeInput(){
+    int data;
+    cin>>data;
+    Node *head=NULL;
+    Node *tail=NULL;
+    while(data!=-1){
+    Node *newNode= new Node(data);
+    if(head==NULL){
+        head =newNode;
+        tail =newNode;
+    }
+    else{
+        Node *temp=head;
+      
+        tail->next=newNode;
+        tail=newNode;
+    }
+    cin>>data;
+
+    }
+    return head;
+
+}
+
+void insert(Node* head,int i,int data){
+    int count=0;
+    Node *temp=head;
+    Node *newNode= new Node(data);
+    while(count<i-1){
+        count++;
+        temp=temp->next;
+    }
+    newNode->next=temp->next;
+    temp->next=newNode;
+}
+
+int main(){
+
+    Node *head= takeInput();
+    print(head);
+    insert(head,2,99); //inserting node with data=99, at 2nd position
+    print(head);
+    
+
+}

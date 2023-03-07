bbb2a569707112aad87e758bd2dd0e39235b6192
Ankesh Gupta <65663130+Ankesh2004@users.noreply.github.com>
2023-03-07 09:23:44 +0530

    Update 1.12 Merging two LL.cpp

--- 1. Linked List/1.12 Merging two LL.cpp
+++ 1. Linked List/1.12 Merging two LL.cpp
@@ -44,45 +44,78 @@ Node *takeInput(){
     return head;
 
 }
-Node *merge(Node *head1, Node *head2) {
-if (head1 == NULL) {
-return head2;
+Node *merge(Node *head1, Node *head2)
+{ // merging via creating a third linked list
+    if (head1 == NULL)
+    {
+        return head2;
+    }
+    if (head2 == NULL)
+    {
+        return head1;
+    }
+    Node *temp1 = head1, *temp2 = head2;
+    Node *mergedList = NULL, *tail = NULL;
+    if (temp1->data <= temp2->data)
+    {
+        mergedList = temp1;
+        tail = temp1;
+        temp1 = temp1->next;
+    }
+    else
+    {
+        mergedList = temp2;
+        tail = temp2;
+        temp2 = temp2->next;
+    }
+    while (temp1 != NULL && temp2 != NULL)
+    {
+        if (temp1->data <= temp2->data)
+        {
+            tail->next = temp1;
+            tail = temp1;
+            temp1 = temp1->next;
+        }
+        else
+        {
+            tail->next = temp2;
+            tail = temp2;
+            temp2 = temp2->next;
+        }
+    }
+    if (temp1 != NULL)
+    {
+        tail->next = temp1;
+    }
+    else
+    {
+        tail->next = temp2;
+    }
+    return mergedList;
 }
-if (head2 == NULL) {
-return head1;
+
+Node *mergeList(Node *head1, Node *head2)
+{ // merging without a 3rd LL
+    if (head1 == NULL)
+    {
+        return head2;
+    }
+    if (head2 == NULL)
+    {
+        return head1;
+    }
+    if (head1->data < head2->data)
+    {
+        head1->next = mergeList(head1->next, head2);
+        return head1;
+    }
+    else
+    {
+        head2->next = mergeList(head1, head2->next);
+        return head2;
+    }
 }
-Node *temp1 = head1, *temp2 = head2;
-Node *mergedList = NULL, *tail = NULL;
-if (temp1->data <= temp2->data) {
-mergedList = temp1;
-tail = temp1;
-temp1 = temp1->next;
-}
-else {
-mergedList = temp2;
-tail = temp2;
-temp2 = temp2->next;
-}
-while (temp1 != NULL && temp2 != NULL) {
-if (temp1->data <= temp2->data) {
-tail->next = temp1;
-tail = temp1;
-temp1 = temp1->next;
-}
-else {
-tail->next = temp2;
-tail = temp2;
-temp2 = temp2->next;
-}
-}
-if (temp1 != NULL) {
-tail->next = temp1;
-}
-else {
-tail->next = temp2;
-}
-return mergedList;
-}
+
 
 
 
